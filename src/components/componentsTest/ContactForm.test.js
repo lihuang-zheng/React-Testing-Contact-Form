68a0d7e6be9cfbe840f0498947f66bc3ee01ec2b
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-13 17:49:27 -0600

    fixed errors.

--- src/components/componentsTest/ContactForm.test.js
+++ src/components/componentsTest/ContactForm.test.js
@@ -8,36 +8,36 @@ import ContactForm from "../ContactForm"
 
 // label testing
 test('firstName, lastName, email, message input render', () => {
-    const { getByLabelText } = render(<ContactForm />)
+    const { getByLabelText } = render(<ContactForm />);
 
-    getByLabelText(/firstName/i)
-    getByLabelText(/lastName/i)
-    getByLabelText(/email/i)
-    getByLabelText(/message/i)
+    getByLabelText(/firstName/i);
+    getByLabelText(/lastName/i);
+    getByLabelText(/email/i);
+    getByLabelText(/message/i);
 })
 
-// // Fire Event
-// test('species, age, notes inputs are rendered', () => {
-//     const { getByLabelText, getByText } = render(<ContactForm />)
+// Fire Event
+test('species, age, notes inputs are rendered', () => {
+    const { getByLabelText, getByText } = render(<ContactForm />)
 
-//     // querying for all the input nodes
-//     const firstNameInput = getByLabelText(/firstName/i)
-//     const lastNameInput = getByLabelText(/lastName/i)
-//     const emailInput = getByLabelText(/email/i)
-//     const messageInput = getByLabelText(/message/i)
+    // querying for all the input nodes
+    const firstNameInput = getByLabelText(/firstName/i)
+    const lastNameInput = getByLabelText(/lastName/i)
+    const emailInput = getByLabelText(/email/i)
+    const messageInput = getByLabelText(/message/i)
 
-//     // use the change event to add text to each input
-//     // fire event is that it runs thing in the virtual DOM (page).
-//     fireEvent.change(firstNameInput, { target: { value: 'Test First Name' } })
-//     fireEvent.change(lastNameInput, { target: { value: 'Test Last Name' } })
-//     fireEvent.change(emailInput, { target: { value: 'Test Email' } })
-//     fireEvent.change(messageInput, { target: { value: 'Test Message' } })
+    // use the change event to add text to each input
+    // fire event is that it runs thing in the virtual DOM(page).
+    fireEvent.change(firstNameInput, { target: { value: 'Test First Name' } })
+    fireEvent.change(lastNameInput, { target: { value: 'Test Last Name' } })
+    fireEvent.change(emailInput, { target: { value: 'Test Email' } })
+    fireEvent.change(messageInput, { target: { value: 'Test Message' } })
 
-//     expect(firstNameInput.value).toBe('Test First Name')
-//     expect(lastNameInput.value).toBe('Test Last Name')
-//     expect(emailInput.value).toBe('Test Email')
-//     expect(messageInput.value).toBe('Test Message')
+    expect(firstNameInput.value).toBe('Test First Name')
+    expect(lastNameInput.value).toBe('Test Last Name')
+    expect(emailInput.value).toBe('Test Email')
+    expect(messageInput.value).toBe('Test Message')
 
-//     //click on the button!
-//     fireEvent.click(getByText(/submit/i))
-// });
+    // click on the button!
+    fireEvent.click(getByText(/submit/i))
+});
